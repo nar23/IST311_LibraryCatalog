23a0fefa3abcf902450aff7f7e3bd8323253398a
nar23 <32639628+nar23@users.noreply.github.com>
2017-11-06 11:39:03 -0500

    Update Catalog.java

--- IST311_LibraryCatalog/src/ist311_librarycatalog/Catalog.java
+++ IST311_LibraryCatalog/src/ist311_librarycatalog/Catalog.java
@@ -14,6 +14,7 @@ import java.util.List;
  * the catalog, etc.
  *
  * @author Jennifer A'Harrah <jka5240@psu.edu>
+ * @author Nadia A Rahim<nar23@psu.edu>
  */
 public class Catalog {
 
@@ -31,7 +32,10 @@ public class Catalog {
 
         }
     };
-    
+    /**
+    * This method will display the books in the catalog
+    * It will display the number,names,the size, author etc of the books in the catalog
+    */
     public void displayBooks() {
         System.out.println("------BOOK CATALOG------");
         System.out.println("Number of books: [" + books.size() + "]");
